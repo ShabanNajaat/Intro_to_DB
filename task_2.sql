6f8f969fce6e64194cbb4289a6bb82d825912019
Shaban Najaat <najaatmohammedshaban@gmail.com>
2025-08-11 11:46:30 +0100

    Create task_2.sql

---
+++ task_2.sql
@@ -0,0 +1,49 @@
+-- Use the alx_book_store database
+USE alx_book_store;
+
+-- Create the Authors table
+CREATE TABLE IF NOT EXISTS Authors (
+  author_id INT AUTO_INCREMENT,
+  author_name VARCHAR(215) NOT NULL,
+  PRIMARY KEY (author_id)
+);
+
+-- Create the Books table
+CREATE TABLE IF NOT EXISTS Books (
+  book_id INT AUTO_INCREMENT,
+  title VARCHAR(130) NOT NULL,
+  author_id INT NOT NULL,
+  price DOUBLE NOT NULL,
+  publication_date DATE NOT NULL,
+  PRIMARY KEY (book_id),
+  FOREIGN KEY (author_id) REFERENCES Authors(author_id)
+);
+
+-- Create the Customers table
+CREATE TABLE IF NOT EXISTS Customers (
+  customer_id INT AUTO_INCREMENT,
+  customer_name VARCHAR(215) NOT NULL,
+  email VARCHAR(215) NOT NULL,
+  address TEXT NOT NULL,
+  PRIMARY KEY (customer_id)
+);
+-- Create the Orders table
+CREATE TABLE IF NOT EXISTS Orders (
+  order_id INT AUTO_INCREMENT,
+  customer_id INT NOT NULL,
+  order_date DATE NOT NULL,
+  PRIMARY KEY (order_id),
+  FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
+);
+
+-- Create the Order_Details table
+CREATE TABLE IF NOT EXISTS Order_Details (
+  orderdetailid INT AUTO_INCREMENT,
+  order_id INT NOT NULL,
+  book_id INT NOT NULL,
+  quantity DOUBLE NOT NULL,
+  PRIMARY KEY (orderdetailid),
+  FOREIGN KEY (order_id) REFERENCES Orders(order_id),
+  FOREIGN KEY (book_id) REFERENCES Books(book_id)
+);
+
